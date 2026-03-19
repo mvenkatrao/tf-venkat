2b528b11c76aa8cb52a71956a8ce574002b64dab
mvenkatrao <45077633+mvenkatrao@users.noreply.github.com>
2026-03-19 17:33:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "UbuntuInstance" {
+    count           = 2
     ami             = "ami-0a14f53a6fe4dfcd1"
     instance_type   = "t2.micro"
     security_groups = ["default"]
