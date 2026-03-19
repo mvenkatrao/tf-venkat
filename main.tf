7bdf7ef10bd3dbe408a9a6771d589dfca97f2c9b
mvenkatrao <45077633+mvenkatrao@users.noreply.github.com>
2026-03-19 17:42:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
 provider "aws" {
-    region = "ap-south-1"  
+  region = "ap-south-1"
 }
 
-
 resource "aws_security_group" "securitygroup" {
-    name = "securitygroup"
-    description = "security group for module"
+  name        = "securitygroup"
+  description = "security group for module"
 
-    ingress = {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
