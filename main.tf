26956a1894d353f9623fab9a6fb0ddccbc63b9fc
mvenkatrao <45077633+mvenkatrao@users.noreply.github.com>
2026-03-19 17:20:28 +0530

    Add AWS provider and Ubuntu instance resource

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "UbuntuInstance" {
+    ami             = "ami-0a14f53a6fe4dfcd1"
+    instance_type   = "t2.micro"
+    security_groups = ["default"]
+    key_name = "venkat"
+    
+    tags = {
+        Name = "Terraform-Instance"
+    }
+  
+}
