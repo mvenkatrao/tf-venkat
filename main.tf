f875c3c13e7146ff2f33d7b175fbb74eb2ab3602
mvenkatrao <45077633+mvenkatrao@users.noreply.github.com>
2026-03-19 17:40:00 +0530

    AutoApprove

--- main.tf
+++ main.tf
@@ -2,15 +2,16 @@ provider "aws" {
     region = "ap-south-1"  
 }
 
-resource "aws_instance" "UbuntuInstance" {
-    count           = 2
-    ami             = "ami-0a14f53a6fe4dfcd1"
-    instance_type   = "t2.micro"
-    security_groups = ["default"]
-    key_name = "venkat"
-    
-    tags = {
-        Name = "Terraform-Instance"
+
+resource "aws_security_group" "securitygroup" {
+    name = "securitygroup"
+    description = "security group for module"
+
+    ingress = {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
     }
   
 }
